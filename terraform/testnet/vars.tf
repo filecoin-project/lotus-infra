8361a0cdfa8f9203c8ec0a24f79653767181fc81
ognots <ognots@protonmail.com>
2019-12-03 16:18:32 -0500

    use dnsimple rather than aws for testnet.filecoin.io

--- terraform/testnet/vars.tf
+++ terraform/testnet/vars.tf
@@ -7,12 +7,26 @@ variable "packet_auth_token" {
   description = "packet api token"
 }
 
+variable "dnsimple_token" {
+  description = "the secret dnsimple token"
+}
+
+variable "dnsimple_account" {
+  description = "the secret dnsimple token"
+  default     = "70480"
+}
+
 variable "zone_id" {
   description = "fil-test.net"
 }
 
 variable "lotus_zone_id" {
   description = "lotu.sh"
+}
+
+variable "testnet_domain" {
+  description = "the testnet domain managed in dnsimple"
+  default     = "testnet.filecoin.io"
 }
 
 variable "lotus_copy_binary" {
