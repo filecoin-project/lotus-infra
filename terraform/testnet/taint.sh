0575d4877d7723d2ea33b622d52dbf029184bb9f
Travis Person <travisperson@users.noreply.github.com>
2019-12-11 01:46:05 +0100

    taint script for easy ansible null_resources

---
+++ terraform/testnet/taint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+terraform12 taint 'null_resource.lotus_bootstrap_dfw[0]'
+terraform12 taint 'null_resource.lotus_bootstrap_dfw[1]'
+terraform12 taint 'null_resource.lotus_bootstrap_fra[0]'
+terraform12 taint 'null_resource.lotus_bootstrap_fra[1]'
+terraform12 taint 'null_resource.lotus_bootstrap_sin[0]'
+terraform12 taint 'null_resource.lotus_bootstrap_sin[1]'
+terraform12 taint 'null_resource.lotus_fountain'
