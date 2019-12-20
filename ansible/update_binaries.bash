6e97ca92b9f7410adfe1c915e1135a90b7d9e2f5
Oggy Nots <47555507+ognots@users.noreply.github.com>
2019-12-20 17:29:26 -0500

    minor updates to update script (#36)
    * minor updates to update script
    
    * change inventory file
    * add stats host
    * include addtitional variables
    
    * flags to toggle different ansible options
    
    makes for easier and more flexible use of this script
    
    * only run build when copy selected

--- ansible/update_binaries.bash
+++ ansible/update_binaries.bash
@@ -2,18 +2,51 @@
 
 set -xe
 
-UPDATE_BRANCH="master"
+usage() {
+  set +x
+  echo "usage: update_binaries.bash
+    [-b <branch> | --branch <branch>]
+    [-c | --copy]
+    [-r | --restart]
+    [-k | --check]
+    [-h | --help]"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -b | --branch )         shift
+                                branch=$1
+                                ;;
+        -c | --copy )           copy="True"
+                                ;;
+        -r | --restart )        restart="True"
+                                ;;
+        -k | --check )          check="--check"
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+UPDATE_BRANCH="${branch:-"master"}"
+COPY_BINARY="${copy:-"False"}"
+RESTART_LOTUS="${restart:-"False"}"
+ANSIBLE_CHECK_MODE="${check:- ""}"
 LOTUS_SRC=$(mktemp -d)
 
-git fetch origin
+if [ "$COPY_BINARY" = "True" ]; then
+  git clone --branch $UPDATE_BRANCH https://github.com/filecoin-project/lotus.git $LOTUS_SRC
 
-git clone --branch $UPDATE_BRANCH https://github.com/filecoin-project/lotus.git $LOTUS_SRC
+  pushd $LOTUS_SRC
 
-pushd $LOTUS_SRC
+  make clean deps lotus lotus-storage-miner
 
-make clean deps lotus lotus-storage-miner
-
-popd
+  popd
+fi
 
 HOSTS1=(
   t0222.miner.fil-test.net
@@ -24,6 +57,7 @@ HOSTS1=(
 
 HOSTS2=(
   lotus-fountain.yyz.fil-test.net
+  stats.fil-test.net
   lotus-bootstrap-0.dfw.fil-test.net
   lotus-bootstrap-1.dfw.fil-test.net
   lotus-bootstrap-0.fra.fil-test.net
@@ -33,24 +67,27 @@ HOSTS2=(
 )
 
 for HOST in "${HOSTS1[@]}"; do
-    ansible-playbook --vault-password-file .vault_password            \
-    -i testnet_hosts lotus_bootstrap_miner_update_binaries.yml        \
-    -e ansible_ssh_user=ubuntu                                        \
+    ansible-playbook lotus_bootstrap_miner_update_binaries.yml        \
     -e lotus_binary_src="${LOTUS_SRC}/lotus"                          \
     -e lotus_miner_binary_src="${LOTUS_SRC}/lotus-storage-miner"      \
+    -e lotus_copy_binary=$COPY_BINARY                                 \
+    -e lotus_miner_copy_binary=$COPY_BINARY                           \
+    -e lotus_daemon_restart=$RESTART_LOTUS
     --limit $HOST                                                     \
+    $ANSIBLE_CHECK_MODE                                               \
     $@
 
     read  -n 1 -p "Press any key to continue"
 done
 
 for HOST in "${HOSTS2[@]}"; do
-    ansible-playbook --vault-password-file .vault_password            \
-    -i testnet_hosts lotus_bootstrap_miner_update_binaries.yml        \
-    -e ansible_ssh_user=root                                          \
+    ansible-playbook lotus_bootstrap_miner_update_binaries.yml        \
     -e lotus_binary_src="${LOTUS_SRC}/lotus"                          \
     -e lotus_miner_binary_src="${LOTUS_SRC}/lotus-storage-miner"      \
+    -e lotus_copy_binary=$COPY_BINARY                                 \
+    -e lotus_daemon_restart=$RESTART_LOTUS                            \
     --limit $HOST                                                     \
+    $ANSIBLE_CHECK_MODE                                               \
     $@
 
     read  -n 1 -p "Press any key to continue"
