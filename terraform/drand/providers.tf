1757c94acdabf87d012659768658535ad2dd67f3
Travis Person <travisperson@users.noreply.github.com>
2020-05-01 17:28:48 +0000

    terraform: add drand

---
+++ terraform/drand/providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "filecoin"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "filecoin-terraform-state"
+    key            = "drand-testnet-us-east-1.tfstate"
+    dynamodb_table = "filecoin-terraform-state"
+    region         = "us-east-1"
+    profile        = "filecoin"
+  }
+}
