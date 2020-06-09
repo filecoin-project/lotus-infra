c2b3c0b759484dc6760f14f225973a10060c5169
Travis Person <travisperson@users.noreply.github.com>
2020-06-09 19:02:15 +0000

    interopnet: enable fountain

--- ansible/setup_interopt_miner.bash
+++ ansible/setup_interopt_miner.bash
@@ -205,4 +205,5 @@ ansible-playbook -i $hostfile lotus_fountain.yml                                
                  -e lotus_service_state=started                                                                \
                  -e lotus_daemon_bootstrap=false                                                               \
                  -e lotus_import_wallet=true                                                                   \
+                 -e lotus_fountain_enabled=true                                                                \
                  "$@"
