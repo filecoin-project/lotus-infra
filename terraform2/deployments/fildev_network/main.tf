8412e7b3f4bb2554ae3144c8db581761899d7041
Travis Person <travisperson@users.noreply.github.com>
2020-07-16 18:00:17 +0000

    calibration network

--- terraform2/deployments/fildev_network/main.tf
+++ terraform2/deployments/fildev_network/main.tf
@@ -8,8 +8,8 @@
 locals {
   domain_name     = "fildev.network"
   cidr            = "10.0.0.0/16"
-  public_subnets  = ["10.0.2.0/24", "10.0.3.0/24", "10.0.255.0/24"]
-  private_subnets = ["10.0.128.0/24", "10.0.129.0/24"]
+  public_subnets  = ["10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets = ["10.0.128.0/24", "10.0.129.0/24", "10.0.130.0/24"]
 }
 
 module "nerpanet" {
@@ -62,6 +62,31 @@ module "butterflynet" {
   private_subnet_cidr         = module.fildev_network_vpc.database_subnets_cidr_blocks[1]
 }
 
+module "calibrationnet" {
+  source                      = "../../modules/devnet"
+  name                        = "calibration"
+  zone_id                     = aws_route53_zone.fildev_domain.id
+  ami                         = "ami-053bc2e89490c5ab7"
+  key_name                    = "filecoin"
+  vpc_id                      = module.fildev_network_vpc.vpc_id
+  environment                 = "prod"
+  bootstrapper_count          = 4
+  preminer_count              = 3
+  scratch_count               = 2
+  toolshed_instance_type      = "m5a.large"
+  preminer_instance_type      = "p3.2xlarge"
+  bootstrapper_instance_type  = "m5a.large"
+  scratch_instance_type       = "m5a.large"
+  preminer_volume_size        = 256
+  preminer_iam_profile        = aws_iam_instance_profile.sectors.id
+  database_subnet_group       = module.fildev_network_vpc.database_subnet_group
+  public_subnet_id            = module.fildev_network_vpc.public_subnets[2]
+  public_subnet_cidr          = module.fildev_network_vpc.public_subnets_cidr_blocks[2]
+  private_subnet_id           = module.fildev_network_vpc.database_subnets[2]
+  private_subnet_cidr         = module.fildev_network_vpc.database_subnets_cidr_blocks[2]
+}
+
+/*
 module "seeding" {
   source                      = "../../modules/seeding"
   name                        = "seeding"
@@ -70,9 +95,10 @@ module "seeding" {
   key_name                    = "filecoin"
   vpc_id                      = module.fildev_network_vpc.vpc_id
   environment                 = "prod"
-  presealer_count             = 6
+  presealer_count             = 0
   presealer_instance_type     = "c5.24xlarge"
   presealer_iam_profile       = aws_iam_instance_profile.presealer.id
-  public_subnet_id            = module.fildev_network_vpc.public_subnets[2]
-  public_subnet_cidr          = module.fildev_network_vpc.public_subnets_cidr_blocks[2]
+  public_subnet_id            = module.fildev_network_vpc.public_subnets[]
+  public_subnet_cidr          = module.fildev_network_vpc.public_subnets_cidr_blocks[]
 }
+*/
