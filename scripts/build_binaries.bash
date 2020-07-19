1f9c826ba04a18306a9383d410fb56c1930d0058
Travis Person <travisperson@users.noreply.github.com>
2020-07-19 23:06:57 +0000

    update all changed names after ux rename in lotus

--- scripts/build_binaries.bash
+++ scripts/build_binaries.bash
@@ -75,7 +75,7 @@ if [ ! -z "$GOPATH" ]; then
 fi
 
 if [ $# -eq 0 ]; then
-  buildlist=(lotus lotus-storage-miner lotus-seal-worker lotus-seed lotus-shed fountain stats chainwatch)
+  buildlist=(lotus lotus-miner lotus-seal-worker lotus-seed lotus-shed lotus-fountain lotus-stats lotus-chainwatch)
 else
   buildlist="$@"
 fi
