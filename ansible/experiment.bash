4459d378f7e8ddbd102e2a80a1b409fce7076349
Cory Schwartz <cory@protocol.ai>
2020-07-21 14:24:37 -0700

    dont clear stats

--- ansible/experiment.bash
+++ ansible/experiment.bash
@@ -6,8 +6,8 @@ build_flags="${buildflags:-"-f"}"
 lotus_src="${src:-"$GOPATH/src/github.com/filecoin-project/lotus"}"
 sentinel_src="${ssrc:-"$GOPATH/src/github.com/filecoin-project/sentinel"}"
 
-#../scripts/build_binaries.bash -s "$lotus_src" ${build_flags}
-#../scripts/build_binaries.bash -s "$sentinel_src" -- telegraf
+# ../scripts/build_binaries.bash -s "$lotus_src" ${build_flags}
+# ../scripts/build_binaries.bash -s "$sentinel_src" -- telegraf
 
 
 
@@ -20,13 +20,7 @@ ansible-playbook -i $hostfile experiment.yml \
     -e lotus_fountain_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-fountain"    \
     -e stats_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-stats"                \
     -e telegraf_binary_src="$GOPATH/src/github.com/filecoin-project/sentinel/build/telegraf"       \
-    -e lotus_reset=yes -e lotus_miner_reset=yes -e stats_reset=yes                                 \
+    -e lotus_reset=yes -e lotus_miner_reset=yes -e stats_reset=no \
 
-
-
-# failed: [toolshed-1.butterfly.fildev.network] (item=lotus_path) => {"ansible_loop_var": "item", "changed": false, "item": "lotus_path", "msg": "Variable 'lotus_path' is not defined"}
-# failed: [toolshed-1.butterfly.fildev.network] (item=lotus_golog_file) => {"ansible_loop_var": "item", "changed": false, "item": "lotus_golog_file", "msg": "Variable 'lotus_golog_file' is not defined"}
-# failed: [toolshed-1.butterfly.fildev.network] (item=lotus_proof_params_path) => {"ansible_loop_var": "item", "changed": false, "item": "lotus_proof_params_path", "msg": "Variable 'lotus_proof_params_path' is not defined"}
-# failed: [toolshed-1.butterfly.fildev.network] (item=lotus_libp2p_port) => {"ansible_loop_var": "item", "changed": false, "item": "lotus_libp2p_port", "msg": "Variable 'lotus_libp2p_port' is not defined"}
 
 
