7e0e28aac45f3788693aca3ea15eb53f0be782de
Oggy Nots <47555507+ognots@users.noreply.github.com>
2020-11-21 10:22:34 -0500

    Feat/storage miner k8s (#186)
    * ansible for spinning up k8s
    
    playsbooks, roles and files for kubernetes
    
    * k8s infra in packet
    
    hosts and vlans attachments
    
    * use default flannel cidr
    
    * add DNS records and change node ips
    
    * create dns records in terraform and add to ansible inventory
    * explicitly set vlan ips in kubelet for node ip
    
    * fix UFW rules breaking flannel
    
    * misc networking fixes and tf updates
    
    * up to date hosts file for currently deployed cluster
    * safer vlan and vlan2 updating
    * temporary c2.medium hosts in terraform due to bug in c3.mediums
    * terraform state matching existing setup
    * dns for packet machines
    
    * add custom c3.mediums for workers
    
    * use 10 reservations in tform and update ansible inventory
    * unrelated change to labels and taints file - taint generic node because it's being used for nat gateway
    
    * WIP
    
    * spacerace updates to k8s miner infra
    
    * destroy one of the expensive gpu machines
    
    we don't need it as we've hit 10TB and don't need to onboard storage as fast

---
+++ terraform/storage-miner-k8s-dfw2/vars.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "packet project id"
+  default     = ""
+}
+
+variable "packet_auth_token" {
+  description = "packet api token"
+}
+
+variable "zone_id" {
+  default = "fil-test.net"
+}
