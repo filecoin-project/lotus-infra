258c61f7057415a788254e65c10e8f53056958ae
ognots <ognots@protonmail.com>
2020-12-04 20:36:47 -0500

    reenable s3 state

--- terraform/eks/environments/mainnet-us-east-1/main.tf
+++ terraform/eks/environments/mainnet-us-east-1/main.tf
@@ -1,4 +1,3 @@
-/*
 terraform {
   backend "s3" {
     bucket         = "filecoin-mainnet-terraform-state"
@@ -8,7 +7,6 @@ terraform {
     profile        = "mainnet"
   }
 }
-*/
 
 module "vpc" {
   source = "../../modules/vpc"
