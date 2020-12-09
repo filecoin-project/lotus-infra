3d997fd1b35f3178c6ba63a4d6f9d166294fd5c0
Travis Person <travisperson@users.noreply.github.com>
2020-12-09 20:46:13 +0000

    take down yaf network

--- terraform2/deployments/fildev_network/main.tf
+++ terraform2/deployments/fildev_network/main.tf
@@ -26,10 +26,10 @@ module "yafnet" {
   key_name                    = "filecoin"
   vpc_id                      = module.fildev_network_vpc.vpc_id
   environment                 = "prod"
-  toolshed_count              = 2
-  bootstrapper_count          = 2
-  preminer_count              = 6
-  scratch_count               = 2
+  toolshed_count              = 0
+  bootstrapper_count          = 0
+  preminer_count              = 0
+  scratch_count               = 0
   toolshed_instance_type      = "m5a.xlarge"
   chainwatch_db_instance_type = "db.m5.large"
   chainwatch_password         = var.nerpanet_chainwatch_password
