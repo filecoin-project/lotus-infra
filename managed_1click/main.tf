958323fc564d653cab79c87681c7824bd73b48dd
Cory Schwartz <cory@protocol.ai>
2021-03-31 16:17:19 -0700

    ebs block device

--- managed_1click/main.tf
+++ managed_1click/main.tf
@@ -51,6 +51,7 @@ variable "oneclickusers" {
   default = {
     "willscot" =  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/RMGjrpi7C7g8cwCuYjgjAskc8Cask4BMSwxXwQ+2O6B704dvjrew/WXKSOE53bOVjbr3pLMd5EuWOaYPZlSYgIz0EdOZXzxjB5QJAMmk8pZDERaeAWTUhOiGLA1iG4hBX+LEON0US8LZWNWaj87KbZF+LcdsrBSMbdB83LvYMvgg0VHYuMb5gEUrFz6aABkaKqgYYd578o5/ObT1UqM9QvdpimH/pFhaT8ZhSq/c9b/7exA1GUyRbHeEdNO4GJPCjcVmVD5PVH5Iw1ZYEeEcX49a8hxT7209pwfn5Dv82Mcw8fxHDz/ZBfaqYdY9KhASs2BQ1vq+UkRvZXuy8BI1wgctfF06okYQeFToCMHgCjBJMxlr0SU13XvDsCJSmM9c7nyP4CKqtD0i5V8FsA6V7gK6SYW2KE1iAvXYq6pbY/1IRftErNcBj5SF5ZTS8ePjJ4xIjQGZT8fIyG6eE3baionf8nr5G3aOUvF/ztdfqSEjGMwdhzcba83kFW5hpnwCS09W+NfqSpEenMqyTwLxgFMYLfXC+mx5CCwQAb9sThdQ7SE/sEPkCFJ7Q9618VhnoWtLu1zUMjK4yU6fsym5EC6eb7G8sn68FjhuXNTx72OTvyPcTiTd7Dp5Eg7NdBcn1PNaW8nGX8/JXcAFNVGqJGbTmzXgFP3z7+BSlbSdfw=="
     "alexcruikshank" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDwXg25uubw6iOi49+lsF4SCuOlQueRVsMs+cDbmIDO0TTzrw4Y4znvjm93+6WXmEQyG9JlJdhRjqveGCcD1HbcwX4xjQR8WhPcSH2sOY9Axj9BDwgdBcIYixuvTUADo7QrYlkbfZAlkLeNpR8z9DaYlmG76OrMT38B7LEF9JuyYNu4t4bs4aTx8CSfCmJSBpWGz/jkvaylW35lA/UqE286ILVU5B2MmjgwG5ZRYpJ+2E+knauX+5mOWnoy0AHqAoI4uI8eqOZHwsmKF3ilqvsDKNBmqFgtYTodxxZDQOdQ3JgYWmKOl/0xfkobNRszz6lntzTFWiM9M7YKBweeOYNR"
+    "cory" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDiwx4Vw4oVp/WaxHFqd85KdvQ03E78mUcjD7wr6XRl7xFssVg+NQAoBc1UVKpavrDFcbYHAcEWQbvY//izmloGql1CbhNBEp9pHNHe1QJLChPZDJ0J4ljCHCtWTYQqYXhF5j7qefQjZ/chwbJJIV/47koUKXjJUFW7ohtF07WGr8SGXn3oSrP+8lynj9I7JHRX5XbzcID7qlQ5pX52mCzNkrESrhUi+KIMo0d9i0EyhJKNT+qDVwLBoaqDb5vtLSixNE4LLlsC6xp/YnE3dEY2vLyapfiBddsDxfFQJaqjZ7qlQgsWOuwwDNJOrzOSmLTqZ5wgTG2ViVv32Ngen16bLTH4V2E3h5M6meHFpgZT0lhU8wGP/qmvFmwOvsTMmGu+kmtvs3Pov5B3li7AgOOneYs7/PP74WZL74kg63cZ7yUMAZo7FcTTWwyQhdphCmwDH7ymVIQEUPlfyh85jt2Q2mkBMQc+7f6NhxZVMGc7WYcIDRXmXwyJlDai7xNtTQs= cory@nemesis"
   }
 }
 
@@ -74,4 +75,24 @@ resource "aws_instance" "oneclickinstance" {
   }
   security_groups = [ aws_security_group.alltraffic.name ]
   user_data = file("user_data.sh")
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_size = 1000
+    volume_type = "gp2"
+    delete_on_termination = false
+  }
 }
+
+/* resource "aws_ebs_volume" "oneclickvolume" { */
+/*   for_each = var.oneclickusers */
+/*   availability_zone = aws_instance.oneclickinstance[each.key].availability_zone */
+/*   size = 1000 */
+/*   type = "gp2" */
+/* } */
+
+/* resource "aws_volume_attachment" "oneclickattachment" { */
+/*   for_each = var.oneclickusers */
+/*   device_name = "/dev/sdf" */
+/*   volume_id = aws_ebs_volume.oneclickvolume[each.key].id */
+/*   instance_id = aws_instance.oneclickinstance[each.key].id */
+/* } */
