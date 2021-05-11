9d5f283157b31a05782bc4efa93d5bc238023544
Cory Schwartz <cory@protocol.ai>
2021-05-11 16:50:12 -0700

    use lotus_src

--- ansible/upgrade_fildev_network_binaries.bash
+++ ansible/upgrade_fildev_network_binaries.bash
@@ -28,16 +28,16 @@ lotus_src="${src:-"$GOPATH/src/github.com/filecoin-project/lotus"}"
 ../scripts/build_binaries.bash --src "$lotus_src" ${build_flags} --network $network_flag --build-ffi
 
 # runs all the roles
-ansible-playbook -i $hostfile lotus_devnet_provision.yml                                           \
-    -e lotus_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus"                      \
-    -e lotus_miner_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-miner"          \
-    -e lotus_shed_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-shed"            \
-    -e lotus_seed_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-seed"            \
-    -e lotus_pcr_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-pcr"              \
-    -e lotus_fountain_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-fountain"    \
-    -e stats_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-stats"                \
-    -e chainwatch_binary_src="$GOPATH/src/github.com/filecoin-project/lotus/lotus-chainwatch"      \
-    -e lotus_reset=no -e lotus_miner_reset=no -e stats_reset=no -e lotus_pcr_reset=no              \
-    -e chainwatch_db_reset=no -e chainwatch_reset=no                                               \
-    -e certbot_create_certificate=${create_certificate}                                            \
+ansible-playbook -i $hostfile lotus_devnet_provision.yml                              \
+    -e lotus_binary_src="$lotus_src/lotus"                                            \
+    -e lotus_miner_binary_src="$lotus_src/lotus-miner"                                \
+    -e lotus_shed_binary_src="$lotus_src/lotus-shed"                                  \
+    -e lotus_seed_binary_src="$lotus_src/lotus-seed"                                  \
+    -e lotus_pcr_binary_src="$lotus_src/lotus-pcr"                                    \
+    -e lotus_fountain_binary_src="$lotus_src/lotus-fountain"                          \
+    -e stats_binary_src="$lotus_src/lotus-stats"                                      \
+    -e chainwatch_binary_src="$lotus_src/lotus-chainwatch"                            \
+    -e lotus_reset=no -e lotus_miner_reset=no -e stats_reset=no -e lotus_pcr_reset=no \
+    -e chainwatch_db_reset=no -e chainwatch_reset=no                                  \
+    -e certbot_create_certificate=${create_certificate}                               \
     --diff -v
