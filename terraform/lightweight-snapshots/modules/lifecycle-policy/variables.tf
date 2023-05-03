6fb648e24123e2853ab348153c977e400f83ce6c
GlacierWalrus <114941647+GlacierWalrus@users.noreply.github.com>
2023-05-03 13:27:11 +0000

    Add staging calibration cleanup policy
    Partly fixes #1431

---
+++ terraform/lightweight-snapshots/modules/lifecycle-policy/variables.tf
@@ -0,0 +1,28 @@
+variable "bucket_name" {
+  type        = string
+  description = "The name of the bucket to attach the lifecycle policy to."
+}
+
+variable "expiration_timeframe" {
+  type        = number
+  default     = 30
+  description = "The number of days to keep the objects before expiration."
+}
+
+variable "path_prefix" {
+  type        = string
+  default     = "minimal/"
+  description = "The path prefix to apply the lifecycle policy to."
+}
+
+variable "rule_id" {
+  type        = string
+  default     = "obselete-snapshots"
+  description = "The ID of the lifecycle rule."
+}
+
+variable "status" {
+  type        = string
+  default     = "Enabled"
+  description = "The status of the lifecycle rule."
+}
