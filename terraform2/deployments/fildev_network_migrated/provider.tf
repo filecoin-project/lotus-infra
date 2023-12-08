7a5fa6a3aa4e65804d4e8f075ce745cf8f6f1ede
ognots <ognots@protonmail.com>
2023-12-08 15:55:06 -0500

    WIP

--- terraform2/deployments/fildev_network_migrated/provider.tf
+++ terraform2/deployments/fildev_network_migrated/provider.tf
@@ -2,15 +2,14 @@
 provider "aws" {
   alias = "us-west-2"
   region  = "us-west-2"
-  profile = "mainnet"
 }
 
 provider "aws" {
   alias = "us-east-1"
   region = "us-east-1"
-  profile = "mainnet"
 }
 
+/*
 terraform {
   backend "s3" {
     bucket         = "filecoin-mainnet-terraform-state"
@@ -20,3 +19,4 @@ terraform {
     profile        = "mainnet"
   }
 }
+*/
